9248ea413120a7d4b0b0f75d30059f9df6add5d3
Pedro Santana <pedrosantana@duck.com>
2022-07-29 23:33:35 +0100

    Init hotjar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import { hotjar } from 'react-hotjar';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  hotjar.initialize(3086572, 6)
+  hotjar.identify('USER_ID', { userProperty: 'value' });
+  hotjar.event('button-click');
+  hotjar.stateChange('/my/page');
+
+  return <><Component {...pageProps} /></>;
 }
 
 export default MyApp;
