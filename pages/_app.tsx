8ae6a62844c52eea8d31a4dceee434d720720c07
Pedro Santana <pedrosantana@duck.com>
2022-07-29 23:36:05 +0100

    Init hotjar - window

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { hotjar } from 'react-hotjar';
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  hotjar.initialize(3086572, 6)
-  hotjar.identify('USER_ID', { userProperty: 'value' });
-  hotjar.event('button-click');
-  hotjar.stateChange('/my/page');
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      hotjar.initialize(3086572, 6)
+      hotjar.identify('USER_ID', { userProperty: 'value' });
+      hotjar.event('button-click');
+      hotjar.stateChange('/my/page');
+    }
+      
+  }, []);
 
   return <><Component {...pageProps} /></>;
 }
