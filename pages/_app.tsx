c5b53eef9814a7446942e512af4b8ee0e311a0e2
Pedro Santana <pedrosantana@duck.com>
2022-07-30 00:14:11 +0100

    Add _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,9 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import { hotjar } from 'react-hotjar';
-import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      hotjar.initialize(3086572, 6)
-      hotjar.identify('USER_ID', { userProperty: 'value' });
-      hotjar.event('button-click');
-      hotjar.stateChange('/my/page');
-    }
-      
-  }, []);
 
-  return <><Component {...pageProps} /></>;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
