4f2256fe14f4c23dd3ddcd5686361203fb16b91a
Pedro Santana <pedrosantana@duck.com>
2024-03-20 18:26:35 +0000

    Add new wine group

--- components/CategoryList.tsx
+++ components/CategoryList.tsx
@@ -3,114 +3,121 @@ import Item, { IItem } from "./Item";
 import styles from "../styles/components.module.scss";
 
 interface ICategoryList {
-  title: string;
-  items: any[];
-  style?: any;
-  wines?: boolean;
-  notes: any[];
+	title: string;
+	items: any[];
+	style?: any;
+	wines?: boolean;
+	notes: any[];
 }
 
 const CategoryList: React.FC<ICategoryList> = ({
-  title,
-  items,
-  style,
-  wines,
-  notes,
+	title,
+	items,
+	style,
+	wines,
+	notes,
 }) => {
-  console.log()
+	console.log();
 
+	if (!!wines) {
+		const wineGroups = [
+			"Alentejo",
+			"Bairrada",
+			"Dão",
+			"Rosés",
+			"Vinho Verde",
+			"Açores",
+			"Douro",
+			"Lisboa",
+			"Trás-os-Montes",
+		];
 
-  if (!!wines) {
-    const wineGroups = [
-      "Alentejo",
-      "Bairrada",
-      "Dão",
-      "Rosés",
-      "Vinho Verde",
-      "Açores",
-      "Douro",
-      "Lisboa",
-    ];
+		return (
+			<div style={style}>
+				<h2 style={{ textAlign: "center", padding: "40px 0" }}>{title}</h2>
+				{title === "Brancos" || title === "Tintos" || title.includes("Wine") ? (
+					wineGroups.map((g) => (
+						<div key={g} style={{ width: "100%" }}>
+							<h3 style={{ textAlign: "center", fontSize: 26 }}>
+								{items
+									.filter((p: any) => p.Categoria.value === title)
+									.filter((p: any) => p.Grupo?.value === g).length > 0
+									? g
+									: ""}
+							</h3>
+							<div>
+								{items
+									.filter((p: any) => p.Categoria.value === title)
+									.filter((p: any) => p.Grupo?.value === g)
+									.map((item: any) => (
+										<Item
+											key={item.id}
+											id={item.id}
+											name={item["Nome"]}
+											price={item["Preço"]}
+											gluten={item["Sem Gluten"]}
+											vegan={item["Vegan"]}
+											vegetarian={item["Vegetariano"]}
+											ingredients={item["Ingredientes"]}
+											castas={item["Castas"]}
+										/>
+									))}
+							</div>
+						</div>
+					))
+				) : (
+					<div>
+						<div>
+							{items
+								.filter((p: any) => p.Categoria.value === title)
+								.map((item: any) => (
+									<Item
+										key={item.id}
+										id={item.id}
+										name={item["Nome"]}
+										price={item["Preço"]}
+										gluten={item["Sem Gluten"]}
+										vegan={item["Vegan"]}
+										vegetarian={item["Vegetariano"]}
+										ingredients={item["Ingredientes"]}
+										castas={item["Castas"]}
+									/>
+								))}
+						</div>
+					</div>
+				)}
+			</div>
+		);
+	}
 
-    return (
-      <div style={style}>
-        <h2 style={{ textAlign: "center", padding: "40px 0" }}>{title}</h2>
-        {title === "Brancos" || title === "Tintos" || title.includes("Wine") ? (
-          wineGroups.map((g) => (
-            <div key={g} style={{ width: "100%" }}>
-              <h3 style={{ textAlign: "center", fontSize: 26 }}>
-                {items
-                  .filter((p: any) => p.Categoria.value === title)
-                  .filter((p: any) => p.Grupo?.value === g).length > 0
-                  ? g
-                  : ""}
-              </h3>
-              <div>
-                {items
-                  .filter((p: any) => p.Categoria.value === title)
-                  .filter((p: any) => p.Grupo?.value === g)
-                  .map((item: any) => (
-                    <Item
-                      key={item.id}
-                      id={item.id}
-                      name={item["Nome"]}
-                      price={item["Preço"]}
-                      gluten={item["Sem Gluten"]}
-                      vegan={item["Vegan"]}
-                      vegetarian={item["Vegetariano"]}
-                      ingredients={item["Ingredientes"]}
-                      castas={item["Castas"]}
-                    />
-                  ))}
-              </div>
-            </div>
-          ))
-        ) : (
-          <div>
-            <div>
-              {items
-                .filter((p: any) => p.Categoria.value === title)
-                .map((item: any) => (
-                  <Item
-                    key={item.id}
-                    id={item.id}
-                    name={item["Nome"]}
-                    price={item["Preço"]}
-                    gluten={item["Sem Gluten"]}
-                    vegan={item["Vegan"]}
-                    vegetarian={item["Vegetariano"]}
-                    ingredients={item["Ingredientes"]}
-                    castas={item["Castas"]}
-                  />
-                ))}
-            </div>
-          </div>
-        )}
-      </div>
-    );
-  }
-
-  return (
-    <div style={style} className={styles.categoryContainer}>
-      <h2 style={{ textAlign: "center" }}>{title}</h2>
-      {items
-        .filter((prato: any) => prato.Categoria.value === title)
-        .map((item: any) => (
-          <Item
-            key={item.id}
-            id={item.id}
-            name={item["Nome"]}
-            price={item["Preço"]}
-            gluten={item["Sem Gluten"]}
-            vegan={item["Vegan"]}
-            vegetarian={item["Vegetariano"]}
-            ingredients={item["Ingredientes"]}
-            castas={item["Castas"]}
-          />
-        ))}
-      {!!notes && notes.filter((note: any) => note.Categoria?.value === title).map((n) => <div key={n.id} className={styles.note}><p>{n.Nota}</p></div>)}
-    </div>
-  );
+	return (
+		<div style={style} className={styles.categoryContainer}>
+			<h2 style={{ textAlign: "center" }}>{title}</h2>
+			{items
+				.filter((prato: any) => prato.Categoria.value === title)
+				.map((item: any) => (
+					<Item
+						key={item.id}
+						id={item.id}
+						name={item["Nome"]}
+						price={item["Preço"]}
+						gluten={item["Sem Gluten"]}
+						vegan={item["Vegan"]}
+						vegetarian={item["Vegetariano"]}
+						ingredients={item["Ingredientes"]}
+						castas={item["Castas"]}
+					/>
+				))}
+			{!!notes &&
+				notes
+					.filter((note: any) => note.Categoria?.value === title)
+					.map((n) => (
+						<div key={n.id} className={styles.note}>
+							<p>{n.Nota}</p>
+						</div>
+					))}
+		</div>
+	);
 };
 
 export default CategoryList;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,30 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-          <Script
-            id="hotjar"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(h,o,t,j,a,r){
-                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                    h._hjSettings={hjid:3086572,hjsv:6};
-                    a=o.getElementsByTagName('head')[0];
-                    r=o.createElement('script');r.async=1;
-                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                    a.appendChild(r);
-                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-              `,
-            }}
-          ></Script>
-        </body>
-      </Html>
-    )
-  }
+	return (
+		<Html>
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
